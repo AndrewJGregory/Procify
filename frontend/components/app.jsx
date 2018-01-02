0e54e81fb0e6116e5e619dd02f493f99aad0813f
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-01-02 10:53:03 -0500

    write app, root components

---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <main id='app'>
+    <h1>
+      Procify from a react component!
+    </h1>
+  </main>
+);
+
+export default App;
--- frontend/procify.jsx
+++ frontend/procify.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import * as sessionActions from './actions/session_actions';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore();
-  // TESTING START
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.login = sessionActions.login;
-  window.signup = sessionActions.signup;
-  window.logout = sessionActions.logout;
-  // TESTING END
-  ReactDOM.render(<h1>Procify!</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
