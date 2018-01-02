3613c680fa29789d25a6458de6bfe6ef4e088479
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-01-02 10:42:03 -0500

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+user_one = User.create(username: 'Andrew', password: 'password')
+user_two = User.create(username: 'cheryl', password: 'threecookies')
