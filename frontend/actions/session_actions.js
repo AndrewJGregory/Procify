8d947579aa4f24290f9920c44d6af24cae71cbf4
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-01-02 10:46:58 -0500

    refactor error handling

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -19,23 +19,21 @@ export const receiveErrors = errors => {
 export const login = user => dispatch => {
   return SessionApiUtil.login(user).then(currentUser => {
     return dispatch(receiveCurrentUser(currentUser));
-  }, (errors) => {
-    return dispatch(receiveErrors(errors.responseJSON));
-  });
+  }, (errors) => _handleErrors(dispatch, errors));
 };
 
 export const signup = user => dispatch => {
   return SessionApiUtil.signup(user).then(currentUser => {
     return dispatch(receiveCurrentUser(currentUser));
-  }, (errors) => {
-    return dispatch(receiveErrors(errors.responseJSON));
-  });
+  }, (errors) => _handleErrors(dispatch, errors));
 };
 
 export const logout = () => dispatch => {
   return SessionApiUtil.logout().then(res => {
     return dispatch(receiveCurrentUser(null));
-  }, (errors) => {
-    return dispatch(receiveErrors(errors.responseJSON));
-  });
+  }, (errors) => _handleErrors(dispatch, errors));
 };
+
+const _handleErrors = (dispatch, errors) => (
+  dispatch(receiveErrors(errors.responseJSON))
+);
