d5cbf874c587eecbe5d4673bc2a61a4d9adf0ccf
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-01-03 14:00:10 -0500

    add letter spacing to session links, increase randomness of guest name

--- frontend/components/greeting_container.js
+++ frontend/components/greeting_container.js
@@ -3,7 +3,7 @@ import Greeting from './greeting';
 import { signup } from '../actions/session_actions';
 
 const mapDispatchToProps = dispatch => {
-  const randomNum = parseInt(Math.random()*1000);
+  const randomNum = parseInt(Math.random()*100000);
   const username = 'Guest'.concat(randomNum);
   const password = 'abcdefghij';
   const user = {username, password};
