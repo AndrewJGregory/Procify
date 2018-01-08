bf11eb72ed133b675a9e9d75d4b352dd8c2099b1
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-01-08 09:02:55 -0500

    fix #3

--- frontend/reducers/song_reducer.js
+++ frontend/reducers/song_reducer.js
@@ -1,13 +1,12 @@
 import { RECEIVE_SONGS, RECEIVE_SONG } from '../actions/song_actions';
 import { RECEIVE_NEW_PLAYLIST } from '../actions/playlist_actions';
-import { merge } from 'lodash';
 
 const songReducer = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_SONGS:
-    return merge({}, state, action.songs);
+    return Object.assign({}, state, action.songs);
     case RECEIVE_SONG:
-    return merge({}, state, {[action.song.id]: action.song});
+    return Object.assign({}, state, {[action.song.id]: action.song});
     case RECEIVE_NEW_PLAYLIST:
     return Object.assign({}, state, action.payload.songs);
     default:
