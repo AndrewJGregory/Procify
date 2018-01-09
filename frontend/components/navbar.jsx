7ef614111da74f5761ce53854acd0559c6736b9f
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-01-09 12:18:49 -0500

    add animate css, style navbar

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { NewPlaylistFormContainer } from './playlist_form_container';
 import * as navbarUtil from '../util/navbar_util.jsx';
+import shortid from 'shortid';
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -20,9 +21,9 @@ class NavBar extends React.Component {
 
     const type = this.props.type;
     const lis = ['playlists', 'tracks', 'albums', 'artists'].map(navbarWord => (
-      navbarUtil.generateLi(type, navbarWord)
+      navbarUtil.generateLi(type, navbarWord, shortid.generate())
     ));
-    
+
     return (
       <section className='nav-bar'>
         {form}
--- frontend/util/navbar_util.jsx
+++ frontend/util/navbar_util.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const generateLi = (type, navbarWord) => {
+export const generateLi = (type, navbarWord, key) => {
   let activeClass = '';
   if (type === navbarWord) {
     activeClass = 'active-nav-link';
   }
-  return (
-    <li className={`nav-link ${activeClass}`}>
-    <Link to={`/collection/${navbarWord}`}>{`${navbarWord}`}</Link>
-    </li>
-  );
+  if (navbarWord === 'tracks') {
+    return (
+      <li className={`nav-link ${activeClass}`} key={key}>
+        <Link to={`/collection/${navbarWord}`}>songs</Link>
+      </li>
+    );
+  } else {
+    return (
+      <li className={`nav-link ${activeClass}`} key={key}>
+        <Link to={`/collection/${navbarWord}`}>{`${navbarWord}`}</Link>
+      </li>
+    );
+  }
 };
