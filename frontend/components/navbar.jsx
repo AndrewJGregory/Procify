b6c8d87ebe2c30efe507df54e504937534f47554
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-01-09 10:16:04 -0500

    position nav bar, new playlist btn

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -23,24 +23,28 @@ class NavBar extends React.Component {
     return (
       <section className='nav-bar'>
         {form}
-        <ul id="nav-bar-ul">
-          <li className='nav-link'>
-            <Link to='/collection/playlists'>Playlists</Link>
-          </li>
-          <li className='nav-link'>
-            <Link to='/collection/tracks'>Songs</Link>
-          </li>
-          <li className='nav-link'>
-            <Link to='/collection/albums'>Albums</Link>
-          </li>
-          <li className='nav-link'>
-            <Link to='/collection/artists'>Artists</Link>
-          </li>
+        <div className='nav-bar-ul-container'>
+          <ul id="nav-bar-ul">
+            <li className='nav-link'>
+              <Link to='/collection/playlists'>Playlists</Link>
+            </li>
+            <li className='nav-link'>
+              <Link to='/collection/tracks'>Songs</Link>
+            </li>
+            <li className='nav-link'>
+              <Link to='/collection/albums'>Albums</Link>
+            </li>
+            <li className='nav-link'>
+              <Link to='/collection/artists'>Artists</Link>
+            </li>
+          </ul>
+        </div>
+        <div className='new-playlist-btn-container'>
           <button className='session-link new-playlist-link'
             onClick={this.displayForm}>
             new playlist
           </button>
-        </ul>
+        </div>
       </section>
     );
   }
