219f0e187b663e49dee985ca93c71b6396f96f9e
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-01-10 08:20:05 -0500

    stlye sidebar

--- frontend/components/sidebar.jsx
+++ frontend/components/sidebar.jsx
@@ -9,10 +9,22 @@ class Sidebar extends React.Component {
   render() {
     return (
       <aside id="sidebar">
-
-        <Link to='/settings/account'>
-          {this.props.username}
-        </Link>
+        <div className='sidebar-top-links-container'>
+          <ul className='sidebar-top-links'>
+            <li>Procify img</li>
+            <div className='search-bar-container'>
+              <li className='search-bar clickable'>Search</li>
+              <i class="fa fa-search" aria-hidden="true"></i>
+            </div>
+            <li className='sidebar-link clickable'>Home</li>
+            <li className='sidebar-link sidebar-music-link clickable'>Your Music</li>
+          </ul>
+        </div>
+        <div className='bottom-sidebar'>
+          <Link to='/settings/account'>
+            {this.props.username}
+          </Link>
+        </div>
       </aside>
     );
   }
