d61f37ac4a64ee616a9255decf85926924822681
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-01-15 21:37:16 -0500

    add prod plugins to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,8 @@ var devPlugins = []; // if using any plugins for development
 
 var prodPlugins = [
   new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
 ];
 
 plugins = plugins.concat(
