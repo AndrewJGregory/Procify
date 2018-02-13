d1dee8369751f6be304c34d6e7926f2135615c21
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-02-12 20:00:06 -0500

    add all model validations test for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,13 +15,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject { build(:user) }
   describe 'validation tests:' do
-    describe 'username' do
-      it { should validate_presence_of(:username).with_message("Please enter your Procify username.") }
-
-      subject { build(:user) }
-      it { should validate_uniqueness_of(:username).with_message("Username has already been taken. Try another username.")}
-    end
-
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+    it { should validate_presence_of(:birthday).with_message('When were you born?') }
+    it { should validate_presence_of(:email).with_message('Please enter your email.') }
+    it { should validate_presence_of(:username).with_message('Please enter your Procify username.') }
+    it { should validate_uniqueness_of(:username).with_message('Username has already been taken. Try another username.')}
+    it {
+      should validate_length_of(:password).
+      is_at_least(6).
+      with_message('Your password is too short. Minimum is 6 characters.')
+    }
   end
+
 end
