6f370a1f226e945a965f7c53a199f2adc042dd20
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-02-13 03:28:00 -0500

    add playlist song unit tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -28,3 +28,10 @@ FactoryBot.define do
     img_url 'www.imgurl.com'
   end
 end
+
+FactoryBot.define do
+  factory :playlist_song do
+    playlist_id 1
+    song_id 1
+  end
+end
--- spec/models/playlist_song_spec.rb
+++ spec/models/playlist_song_spec.rb
@@ -9,3 +9,18 @@
 #  updated_at  :datetime         not null
 #
 
+require 'rails_helper'
+
+RSpec.describe PlaylistSong, type: :model do
+  subject { build(:playlist_song) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:playlist_id) }
+    it { should validate_presence_of(:song_id) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:song) }
+    it { should belong_to(:playlist) }
+  end
+end
