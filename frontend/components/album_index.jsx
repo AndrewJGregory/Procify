cb0ee333156ff286036f6c65bf998bf1cf9c02aa
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-02-15 18:19:11 -0500

    make album, artist index dumb components

--- frontend/components/album_index.jsx
+++ frontend/components/album_index.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import AlbumIndexItemContainer from './album_index_item_container';
 
-class AlbumIndex extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const albums = this.props.albums.map(album => {
+const AlbumIndex = ({ albums }) => {
+    const albumIndexItems = albums.map(album => {
       return <AlbumIndexItemContainer key={album.id} album={album} />;
     });
 
     return (
       <section className='square-index-content'>
         <ul>
-          {albums}
+          {albumIndexItems}
         </ul>
       </section>
     );
-  }
-}
+};
 
 export default AlbumIndex;
--- frontend/components/artist_index.jsx
+++ frontend/components/artist_index.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import ArtistIndexItem from './artist_index_item';
 
-class ArtistIndex extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const artists = this.props.artists.map(artist => {
+const ArtistIndex = ({ artists }) => {
+    const artistIndexItems = artists.map(artist => {
       return <ArtistIndexItem artist={artist} key={artist.id} />;
     });
 
     return (
       <section className='artist-index' >
         <ul>
-          {artists}
+          {artistIndexItems}
         </ul>
       </section>
     );
-  }
-}
+};
 
 export default ArtistIndex;
