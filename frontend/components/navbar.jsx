5a9e9f2ab17175cc74cd0f83079f2497b9baf71e
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-02-18 08:55:20 -0500

    remove navbar container

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import * as navbarUtil from '../util/navbar_util.jsx';
-import shortid from 'shortid';
-import NewPlaylistFormBtnContainer from './new_playlist_form_btn_container';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import * as navbarUtil from "../util/navbar_util.jsx";
+import shortid from "shortid";
+import NewPlaylistFormBtnContainer from "./new_playlist_form_btn_container";
 
-const NavBar = (props) => {
-  const type = props.type;
-  const lis = ['playlists', 'tracks', 'albums', 'artists'].map(navbarWord => (
-    navbarUtil.generateLi(type, navbarWord, shortid.generate())
-  ));
+const NavBar = props => {
+  const { category, type } = props.match.params;
+  const lis = ["playlists", "tracks", "albums", "artists"].map(navbarWord =>
+    navbarUtil.generateLi(type, category, navbarWord, shortid.generate())
+  );
 
   return (
-    <section className='nav-bar'>
-        <div className='nav-bar-ul-container'>
-          <div className='new-playlist-btn-container'>
+    <section className="nav-bar">
+      <div className="nav-bar-ul-container">
+        <div className="new-playlist-btn-container">
           <NewPlaylistFormBtnContainer />
         </div>
-        <ul id="nav-bar-ul">
-          {lis}
-        </ul>
+        <ul className="nav-bar-ul">{lis}</ul>
       </div>
     </section>
   );
 };
 
-export default NavBar;
+export default withRouter(NavBar);
--- frontend/util/navbar_util.jsx
+++ frontend/util/navbar_util.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export const generateLi = (type, navbarWord, key) => {
-  let activeClass = '';
+export const generateLi = (type, category, navbarWord, key) => {
+  let activeClass = "";
   if (type === navbarWord) {
-    activeClass = 'active-nav-link';
+    activeClass = "active-nav-link";
   }
-  if (navbarWord === 'tracks') {
+
+  if (navbarWord === "tracks") {
     return (
       <li className={`nav-link ${activeClass}`} key={key}>
-        <Link to={`/collection/${navbarWord}`}>songs</Link>
+        <Link to={`/${category}/${navbarWord}`}>songs</Link>
       </li>
     );
   } else {
     return (
       <li className={`nav-link ${activeClass}`} key={key}>
-        <Link to={`/collection/${navbarWord}`}>{`${navbarWord}`}</Link>
+        <Link to={`/${category}/${navbarWord}`}>{`${navbarWord}`}</Link>
       </li>
     );
   }
