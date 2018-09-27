03a4057fbe90ee3f3664ed36492a7777611b8a77
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-09-27 09:39:22 -0400

    remove unneeded column from songs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180927133642) do
+ActiveRecord::Schema.define(version: 20180927133838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 20180927133642) do
 
   create_table "songs", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "playlist_id"
     t.integer "artist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
